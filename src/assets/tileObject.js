00dd6bff0ff4302c61f2de35d51ec5d45f841bf3
Jonathan-Boesel <jboesel@vt.edu>
2019-12-13 00:14:52 -0500

    Added flashplayslive to portfolio

--- src/assets/tileObject.js
+++ src/assets/tileObject.js
@@ -1,15 +1,27 @@
 
+import flashplays from './images/flashplays.png'
 import jamclash2 from './images/jamclash2.jpg';
 import ryde from './images/Ryde.png';
 import mealcrave from './images/mealcravenew.png';
 import mario from './images/mario.png';
 import rpg from './images/RPG.png';
-import hangman from './images/Hangman.jpg';
+// import hangman from './images/Hangman.jpg';
 
 
 export default 
-[{
+[   {
         'key': '1',
+        'title': 'Flash Plays Live',
+        'description': 'WordPress Site Build for a client',
+        'description2': 'Fully responsive site built with general specifications set by Flash Plays Live app designer. The site allows users to quickly navigate to section they are interested in, view privacy policy, contact the company, download the app, or apply to be a brand rep.',
+        'builtWith': 'Built using: HTML, CSS, JavaScript, Wordpress, and plugins to handle security and contact portals',
+        'howTo': 'How to use: Navigate the home page by scrolling or clicking the various tabs in the menu at the top of the screen',
+        "image": flashplays,
+        "link": "some Link",
+        "url": "https://www.flashplayslive.com//"
+    },
+    {
+        'key': '2',
         'title': 'Jam Clash',
         'description': 'AWS based social site for musicians',
         'description2': 'Full stack social media app for musicians built with a team. When a user creates an account their location is saved along with their desired musician related information. The user can then search for other musicians with a variety of paramaters that make AJAX calls to an AWS database of users to find people to jam with.',
@@ -20,7 +32,7 @@ export default
         "url": "https://jamclash.herokuapp.com/"
     },
     {
-        'key': '2',
+        'key': '3',
         'title': 'Ryde or Drive',
         'description': "Ride sharing comparison using API's",
         'description2': "A ride sharing app built with a team. Uses various APIs and scripts to output drive distance, compare the cost of Uber and Lyft, and give the user an estimated cost of driving their own vehicle instead.",
@@ -31,7 +43,7 @@ export default
         "url": "https://jonathan-boesel.github.io/Ryde-or-Dryve/"
     },
     {
-        'key': '3',
+        'key': '4',
         'title': 'mealCrave',
         'description': 'View potential meals submitted by users in MySql and Amazon S3',
         'description2': "A full stack food app built with a team prioritizing dishes the user is craving over a specific restaurant. The goal of the app is to eliminate bias in food searches by only displaying restaurant information after a picture is clicked. Once a meal has been selected the restaurant name can be clicked to find the dish nearest you.",
@@ -42,7 +54,7 @@ export default
         "url": "https://meal-crave.herokuapp.com/"
     },
     {
-        'key': '4',
+        'key': '5',
         'title': 'Trivia Game',
         'description': 'Small trivia game exploring click events and timeouts',
         'description2': 'This trivia game has a “Super Smash Bros. 64” theme from the Nintendo 64 era and presents the user with a series of timed questions that display GIFs upon answering. The number of correct and incorrect answers are saved to a variable and displayed at the end.',
@@ -53,7 +65,7 @@ export default
         "url": "https://jonathan-boesel.github.io/Triviagame/"
     },
     {
-        'key': '5',
+        'key': '6',
         'title': 'Click RPG Game',
         'description': 'Interactive click game testing object oriented programming',
         'description2': 'A small RPG game employing Object Oriented Programming to construct the various available characters. Each character has a set amount of health and attack points. The user’s selected character increases in strength with each attack.',
@@ -63,15 +75,15 @@ export default
         "link": "some Link",
         "url": "https://jonathan-boesel.github.io/week-4-game/"
     },
-    {
-        'key': '6',
-        'title': 'Hangman Game',
-        'description': 'Compare your input to an array of correct keywords',
-        'description2': 'In this Internet themed hangman game various keywords are saved to an array along with the corresponding blank spaces to display to the user. User keystrokes are checked against the current keyword and any blanks are replaced with the correct letter. After a word is guessed correctly or the available guesses reach zero, a new word is randomly selected.',
-        'builtWith': 'Built using: HTML, CSS, and Javascript.',
-        'howTo': 'How to use: Click anywhere in the frame to switch your browser’s focus and start guessing letters by pressing your keyboard.',
-        "image": hangman,
-        "link": "some Link",
-        "url": "https://jonathan-boesel.github.io/Hangman/"
-    },
+    // {
+    //     'key': '6',
+    //     'title': 'Hangman Game',
+    //     'description': 'Compare your input to an array of correct keywords',
+    //     'description2': 'In this Internet themed hangman game various keywords are saved to an array along with the corresponding blank spaces to display to the user. User keystrokes are checked against the current keyword and any blanks are replaced with the correct letter. After a word is guessed correctly or the available guesses reach zero, a new word is randomly selected.',
+    //     'builtWith': 'Built using: HTML, CSS, and Javascript.',
+    //     'howTo': 'How to use: Click anywhere in the frame to switch your browser’s focus and start guessing letters by pressing your keyboard.',
+    //     "image": hangman,
+    //     "link": "some Link",
+    //     "url": "https://jonathan-boesel.github.io/Hangman/"
+    // },
 ]
